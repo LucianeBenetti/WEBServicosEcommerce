25262881c46056b5eaad55df0d7afb8644dc3a0a
80130917 <80130917@MSC36171484.correiosnet.int>
2019-06-06 10:14:23 -0300

    Execicio10 - correção das classes e do banco

--- src/java/controle/DAO/UsuarioDao.java
+++ src/java/controle/DAO/UsuarioDao.java
@@ -71,7 +71,6 @@ public class UsuarioDao {
             ConexaoComBanco.closeStatement(conn);
             ConexaoComBanco.closeConnection(conn);
         }
- System.out.println("usuario DAO: " + usuario);
         return usuario;
     }
 
